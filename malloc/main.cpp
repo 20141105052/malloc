1b40666f24294397adb7b36ad7c92a768c8dabd6
20141105052 <xiaoods3on1@sina.com>
2015-11-18 20:28:29 +0800

    Initial Commit

---
+++ malloc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  malloc
+//
+//  Created by 20141105052 on 15/11/18.
+//  Copyright (c) 2015年 20141105052. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
