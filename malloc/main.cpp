2c4d345b8f1e3eff702f8954a67318021b5b3872
20141105052 <xiaoods3on1@sina.com>
2015-12-02 21:22:15 +0800

    new int动态存储分配函数
    malloc函数在C++中兼容C 在C++中使用new int函数实现动态存储分配，用delete实现内存的释放

--- malloc/main.cpp
+++ malloc/main.cpp
@@ -7,20 +7,24 @@
 //
 
 #include <iostream>
+#define N 5
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     int *a;
     int i,j;
-    int N;
+    //int N;
     int temp;
-    scanf("%d",&N);
-    a=(int *)malloc(N*4);
+    //scanf("%d",&N);
+    //a=(int *)malloc(N*4);
+    a=new int[N];
+    if(a==0)
+        exit(0);
     for(i=0;i<N;i++)
         scanf("%d",&a[i]);
     for(i=0;i<N-1;i++)
     {
-        for(j=0;j<N-1-1;j++)
+        for(j=0;j<N-i-1;j++)
         {
             if(a[j]>a[j+1])
             {
@@ -32,5 +36,6 @@ int main(int argc, const char * argv[]) {
     }
     for(i=0;i<N;i++)
         printf("%d\n",a[i]);
+    delete a;
     return 0;
 }
